3ac118ac23c36bf0045a072ae8f49d9a4ca8bae1
Melese Tamiru <mtamiru@psiet.org>
2024-08-25 15:47:18 +0300

    table

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 
 <template>
    <div>
+       <div><table>
+          <tr><td>bank</td><td>selling</td></tr>
+          <tr><td>addis</td><td>109.8</td></tr>
+
+       </table></div>
       <router-link to="/usd">USD</router-link>|
       <router-link to="/eur">EUR</router-link>
       <router-view/>
